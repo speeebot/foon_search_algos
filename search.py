e13c108ffca631010063f43f523db149869b71b1
speeebot <speeebot@gmail.com>
2022-09-29 12:44:49 -0400

    first pass of informed search algos, not sure if correct yet

--- search.py
+++ search.py
@@ -232,6 +232,39 @@ def DFS(depth, max_depth, goal_node=None, kitchen_items=[]):
 
     return task_tree_units, True
 
+#In case of heuristic 1, if you have multiple path with different motions, choose the
+#path that gives higher success rate of executing the motion successfully. For
+#example, a robot has higher success rate of pouring sliced onion compared to
+#slicing a whole onion. The success rate of each motion is provided in the
+#motion.txt file
+def find_best_success_rate_candidate(candidate_units):
+    motion_file = open('motion.txt', 'r')
+    with open('motion.txt', 'r') as f:
+        lines = f.readlines()
+
+    motions = {}
+
+    for line in lines:
+        motion_name = line.split('\t')[0]
+        motion_rate = line.split('\t')[1]
+        motions[motion_name] = motion_rate
+
+    candidate_motions = {}
+    
+    #print(f"# of candidate units: {len(candidate_units)}")
+    for unit in candidate_units:
+        candidate_motion = foon_functional_units[unit].motion_node
+        candidate_motions[candidate_motion] = motions[candidate_motion]
+
+    best_candidate = max(candidate_motions, key=candidate_motions.get)
+
+    #print(f"selected candidate: {best_candidate}")
+        
+    for unit in candidate_units:
+        if foon_functional_units[unit].motion_node == best_candidate:
+            best_unit = unit
+
+    return best_unit
 
 #https://ai-master.gitbooks.io/heuristic-search/content/what-is-greedy-best-first-search.html
 def search_heuristic1(kitchen_items=[], goal_node=None):
@@ -263,7 +296,113 @@ def search_heuristic1(kitchen_items=[], goal_node=None):
 
             # selecting the first path
             # this is the part where you should use heuristic for Greedy Best-First search
-            selected_candidate_idx = candidate_units[0]
+            #selected_candidate_idx = candidate_units[0]
+            selected_candidate_idx = find_best_success_rate_candidate(candidate_units)
+            # If we find that there are more than one way to get the egg mixture, you can choose the first way you
+            # discover in case of iterative deepening. But, when you are using heuristic, you need to check all the 
+            # candidate functional units and calculate the cost of the heuristic function. Then, you can decide which path to choose
+            
+            # if a functional unit is already taken, do not process it again
+            if selected_candidate_idx in reference_task_tree:
+                continue
+
+            reference_task_tree.append(selected_candidate_idx)
+
+            # all input of the selected functional unit need to be explored
+            for node in foon_functional_units[selected_candidate_idx].input_nodes:
+                node_idx = node.id
+                if node_idx not in items_to_search:
+                    # if in the input nodes, we have bowl contains {onion} and onion, chopped, in [bowl]
+                    # explore only onion, chopped, in bowl
+                    flag = True
+                    if node.label in utensils and len(node.ingredients) == 1:
+                        for node2 in foon_functional_units[selected_candidate_idx].input_nodes:
+                            if node2.label == node.ingredients[0] and node2.container == node.label:
+                                flag = False
+                                break
+                    if flag:
+                        items_to_search.append(node_idx)
+
+    # reverse the task tree
+    reference_task_tree.reverse()
+
+    # create a list of functional unit from the indices of reference_task_tree
+    task_tree_units = []
+    for i in reference_task_tree:
+        task_tree_units.append(foon_functional_units[i])
+
+    return task_tree_units 
+
+#count input objects of candidate unit, including object ingredients
+def count_input_objects(candidate_unit):
+
+    count = 0
+
+    for node in foon_functional_units[candidate_unit].input_nodes:
+        flag = True
+        if node.label in utensils and len(node.ingredients) == 1:
+            for node2 in foon_functional_units[candidate_unit].input_nodes:
+                if node2.label == node.ingredients[0] and node2.container == node.label:
+                    flag = False
+                    break
+        if flag:
+            count += len(node.ingredients)
+
+        if node.label is not None:
+            count += 1
+
+    return count
+        
+        
+
+
+#returns functional_unit index for functional unit that contains the least input objects
+#input objects includes object ingredients
+def find_least_input_objects_candidate(candidate_units):
+
+    candidate_input_dict = {}
+    
+    #print(f"# of candidate units: {len(candidate_units)}")
+    for unit in candidate_units:
+        candidate_input_dict[unit] = count_input_objects(unit)
+
+    best_candidate = min(candidate_input_dict, key=candidate_input_dict.get)
+
+    #print(f"selected candidate: {best_candidate}")
+
+    return best_candidate
+
+def search_heuristic2(kitchen_items=[], goal_node=None):
+    # list of indices of functional units
+    reference_task_tree = []
+
+    # list of object indices that need to be searched
+    items_to_search = []
+
+    # find the index of the goal node in object node list
+    items_to_search.append(goal_node.id)
+
+    # list of item already explored
+    items_already_searched = []
+
+    while len(items_to_search) > 0:
+        current_item_index = items_to_search.pop(0)  # pop the first element
+
+        if current_item_index in items_already_searched:
+            continue
+        else:
+            items_already_searched.append(current_item_index)
+
+        current_item = foon_object_nodes[current_item_index] 
+
+        if not check_if_exist_in_kitchen(kitchen_items, current_item): #if goal node doesnt exist, we find steps to make it, otherwise nothing to do
+
+            candidate_units = foon_object_to_FU_map[current_item_index]
+
+            # selecting the first path
+            # this is the part where you should use heuristic for Greedy Best-First search
+            #selected_candidate_idx = candidate_units[0]
+            selected_candidate_idx = find_least_input_objects_candidate(candidate_units)
             # If we find that there are more than one way to get the egg mixture, you can choose the first way you
             # discover in case of iterative deepening. But, when you are using heuristic, you need to check all the 
             # candidate functional units and calculate the cost of the heuristic function. Then, you can decide which path to choose
@@ -351,10 +490,16 @@ if __name__ == '__main__':
 
         for object in foon_object_nodes:
             if object.check_object_equal(node_object):
-                output_task_tree = search_BFS(kitchen_items, object)
+                #output_task_tree = search_BFS(kitchen_items, object)
+                #save_paths_to_file(output_task_tree,
+                #                   'output_BFS_{}.txt'.format(node["label"]))
+                #output_task_tree = IDS(kitchen_items, object)
+                #save_paths_to_file(output_task_tree,
+                #                   'output_IDS_{}.txt'.format(node["label"]))
+                #output_task_tree = search_heuristic1(kitchen_items, object)
+                #save_paths_to_file(output_task_tree,
+                #                   'output_heuristic1_{}.txt'.format(node["label"]))
+                output_task_tree = search_heuristic2(kitchen_items, object)
                 save_paths_to_file(output_task_tree,
-                                   'output_BFS_{}.txt'.format(node["label"]))
-                output_task_tree = IDS(kitchen_items, object)
-                save_paths_to_file(output_task_tree,
-                                   'output_IDS_{}.txt'.format(node["label"]))
+                                   'output_heuristic2_{}.txt'.format(node["label"]))
                 break
